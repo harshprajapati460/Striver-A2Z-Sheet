62114e493680cb23ba3b8498facf270cd8f654c4
Harsh <115858346+harshprajapati460@users.noreply.github.com>
2023-04-08 16:07:34 +0530

    Added atack

---
+++ Stack/NGR_element.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> NGR(vector<int>&arr,int n){
+    vector<int>ans;
+    stack<int>s;
+    for(int i = n-1; i >= 0;i--){
+         if(s.size() == 0){
+            ans.push_back(-1);
+         }
+         else if(s.size() > 0 && s.top() > arr[i]){
+            ans.push_back(s.top());
+         }
+         else if(s.size() > 0 && s.top() <= arr[i]){
+            while(s.size() > 0 && s.top() <= arr[i]){
+                s.pop();
+            }
+            if(s.size() == 0){
+                ans.push_back(-1);
+            }
+            else {
+                ans.push_back(s.top());
+            }
+         }
+         s.push(arr[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+    }
+
+int main(){
+vector<int>arr = {1,3,2,4};
+int n = arr.size();
+for(auto it:arr){
+    cout<<it<<" ";
+}
+cout<<endl;
+vector<int>ans = NGR(arr,n);
+for(auto it:ans){
+    cout<<it<<" ";
+}
+return 0;
+}
